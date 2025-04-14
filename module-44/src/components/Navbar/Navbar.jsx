f3cb40b56cae15992bed5338d925cfc512d26da9
Risan <borhansiddque19@gmail.com>
2025-04-14 09:35:21 +0600

    Add lucide-react dependency and enhance Navbar component with responsive design

--- module-44/src/components/Navbar/Navbar.jsx
+++ module-44/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "./Link";
+import { AlignJustify, X } from "lucide-react";
 
 const navLinks = [
   {
@@ -29,17 +30,32 @@ const navLinks = [
   },
 ];
 
-const links = navLinks.map(link => <Link link={link}></Link>)
-console.log(links);
-
+const links = navLinks.map((link) => <Link link={link}></Link>);
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="flex justify-between px-3 py-5">
       <h1 className="font-semibold text-2xl font-mono">React App.</h1>
-      <ul className="flex gap-5">{links}</ul>
+      <ul className="gap-5 items-center hidden md:flex">{links}</ul>
+
+      <ul
+        className={`md:hidden absolute z-10 ${
+          open ? "top-10" : "-top-60"
+        } right-12 bg-amber-100 text-gray-800 p-5 duration-700`}
+      >
+        {links}
+      </ul>
+
       <div className="">
-        <button className="btn btn-accent">Click me</button>
+        <button className="btn btn-accent hidden md:block">Click me</button>
+        <div className="md:hidden cursor-pointer">
+          {open ? (
+            <X onClick={() => setOpen(!open)}></X>
+          ) : (
+            <AlignJustify onClick={() => setOpen(!open)} />
+          )}
+        </div>
       </div>
     </nav>
   );
